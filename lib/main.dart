9b8fc1a57e955c48e56f3e0c4f31f4bb226cf5ee
[Denis} <[den_ou@hotmail.com]>
2023-07-15 08:34:22 -0300

    Atualizada sessão de blog

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+
 import 'home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Home',
+      home: HomePage(),
+      initialRoute: '/',
+    );
+  }
+}
