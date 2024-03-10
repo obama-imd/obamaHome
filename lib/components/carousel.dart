c00cb595ed931332da81e80f542081b7713bbcdf
[Denis} <[den_ou@hotmail.com]>
2024-03-10 14:37:05 -0300

    Ajuste responsividade

--- lib/components/carousel.dart
+++ lib/components/carousel.dart
@@ -108,7 +108,8 @@ class Carousel extends StatelessWidget {
                                     height: 150,
                                     width: 150,
                                     decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(100),
+                                        borderRadius:
+                                            BorderRadius.circular(100),
                                         image: DecorationImage(
                                             image: AssetImage(imageCarousel[i]),
                                             fit: BoxFit.cover))),
@@ -160,7 +161,8 @@ class Carousel extends StatelessWidget {
                                                   style: textTheme.labelLarge),
                                               Text(
                                                   'Get answers and advice from people you want it from.',
-                                                  style: textTheme.displayMedium)
+                                                  style:
+                                                      textTheme.displayMedium)
                                             ])),
                                     Spacer(),
                                     TextButton(
@@ -168,14 +170,15 @@ class Carousel extends StatelessWidget {
                                           fixedSize: MaterialStatePropertyAll(
                                               Size(150, 50)),
                                           backgroundColor:
-                                              MaterialStatePropertyAll(onPrimary),
+                                              MaterialStatePropertyAll(
+                                                  onPrimary),
                                           overlayColor:
-                                              MaterialStatePropertyAll(onPrimary),
+                                              MaterialStatePropertyAll(
+                                                  onPrimary),
                                         ),
                                         onPressed: () {},
                                         child: Text('CONTACT US',
                                             style: textTheme.labelMedium)),
-                                
                                   ]))),
                         ),
                       ])),
@@ -183,19 +186,19 @@ class Carousel extends StatelessWidget {
             Padding(
               padding: paddingValues("sideMainPadding", swidth),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     for (int i = 0; i < partnersLogo.length; i++) ...{
                       Container(
                           height: 120,
                           width: swidth * 0.17,
-                          padding:
-                              EdgeInsets.symmetric(vertical: partnersPadding[i]),
+                          padding: EdgeInsets.symmetric(
+                              vertical: partnersPadding[i]),
                           decoration: BoxDecoration(
                               border: Border.all(
                                   width: 6,
-                                  color:
-                                      const Color.fromARGB(255, 231, 228, 228))),
+                                  color: const Color.fromARGB(
+                                      255, 231, 228, 228))),
                           child: Image(
                               image: AssetImage(partnersLogo[i]),
                               fit: BoxFit.contain)),
@@ -205,129 +208,133 @@ class Carousel extends StatelessWidget {
           ] else ...[
             SizedBox(height: 100, width: swidth),
             SizedBox(
-                height: 550,
+                height: 740,
                 width: swidth,
-                child: Stack(children: <Widget>[
+                child: Stack( alignment: Alignment.bottomCenter, children: <Widget>[
                   Column(children: [
                     Container(
                         width: MediaQuery.of(context).size.width,
                         padding: const EdgeInsets.only(top: 50),
-                        height: 550,
+                        height: 650,
                         decoration: const BoxDecoration(
                           image: DecorationImage(
                               image: AssetImage('assets/images/test.png'),
                               fit: BoxFit.cover),
                         ))
                   ]),
-                  Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                    Container(height: 40),
-                    Container(
-                        padding: const EdgeInsets.only(left: 40, top: 50),
-                        child: Text('What Clients Say?',
-                            style: textTheme.displayLarge)),
-                    Container(
-                        padding: const EdgeInsets.only(top: 15, left: 40),
-                        child: Text(
-                          'Here are testimonials from clients..',
-                          style: textTheme.displaySmall,
-                        )),
-                    Container(height: 20),
-                    Row(children: [
-                      Container(width: 40),
-                      SizedBox(
-                          height: 5.0,
-                          width: 82.0,
-                          child: Image.asset('assets/images/img2.jpg',
-                              fit: BoxFit.cover)),
-                    ]),
-                    SizedBox(height: 40, width: swidth),
-                    ImageSlideshow(
-                      width: swidth,
-                      height: 240,
-                      initialPage: 0,
-                      indicatorColor: CoresPersonalizadas.azulObama,
-                      indicatorBackgroundColor: Colors.grey,
-                      onPageChanged: (value) {
-                        print('Page changed: $value');
-                      },
-                      autoPlayInterval: 3500,
-                      isLoop: true,
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        for (int i = 0; i < titleCarousel.length; i++) ...{
-                          Row(children: [
-                            Container(
-                                margin: const EdgeInsets.only(left: 40),
-                                height: 120,
-                                width: 120,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    image: DecorationImage(
-                                        image: AssetImage(imageCarousel[i]),
-                                        fit: BoxFit.cover))),
-                            Container(
-                                padding: const EdgeInsets.only(left: 30),
-                                height: 100,
-                                width: swidth - 160,
-                                child: ListView(children: [
-                                  Text(titleCarousel[i],
-                                      style: textTheme.displayMedium),
-                                  Container(height: 35),
-                                  Text(nameCarousel[i],
-                                      style: textTheme.displaySmall),
-                                  Container(height: 20),
-                                  Text(summaryCarousel[i],
-                                      style: textTheme.displaySmall)
-                                ])),
-                          ]),
-                        },
-                      ],
-                    )
-                  ])
-                ])),
-            SizedBox(width: swidth * 0.8, height: 20),
-            Center(
-                child: Container(
-                    color: CoresPersonalizadas.azulObama,
-                    height: 180,
-                    width: swidth * 0.95,
-                    child: Column(children: [
-                      Container(width: 30),
-                      Row(children: [
-                        Container(width: 15),
-                        const SizedBox(
-                            width: 50,
-                            height: 100,
-                            child: Icon(
-                              Icons.phone,
-                              size: 40,
-                              color: background,
+                        Container(height: 40),
+                        Container(
+                            padding: const EdgeInsets.only(left: 40, top: 50),
+                            child: Text('What Clients Say?',
+                                style: textTheme.displayLarge)),
+                        Container(
+                            padding: const EdgeInsets.only(top: 15, left: 40),
+                            child: Text(
+                              'Here are testimonials from clients..',
+                              style: textTheme.displaySmall,
                             )),
-                        SizedBox(
-                            width: swidth * 0.7,
-                            height: 100,
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text('REQUEST A FREE QUOTE',
-                                      style: textTheme.bodySmall),
-                                  Text(
-                                      'Get answers and advice from people you want ir from.',
-                                      style: textTheme.bodySmall)
-                                ]))
+                        Container(height: 20),
+                        Row(children: [
+                          Container(width: 40),
+                          SizedBox(
+                              height: 5.0,
+                              width: 82.0,
+                              child: Image.asset('assets/images/img2.jpg',
+                                  fit: BoxFit.cover)),
+                        ]),
+                        SizedBox(height: 40, width: swidth),
+                        ImageSlideshow(
+                          width: swidth,
+                          height: 240,
+                          initialPage: 0,
+                          indicatorColor: CoresPersonalizadas.azulObama,
+                          indicatorBackgroundColor: Colors.grey,
+                          onPageChanged: (value) {
+                            print('Page changed: $value');
+                          },
+                          autoPlayInterval: 3500,
+                          isLoop: true,
+                          children: [
+                            for (int i = 0; i < titleCarousel.length; i++) ...{
+                              Row(children: [
+                                Container(
+                                    margin: const EdgeInsets.only(left: 40),
+                                    height: 120,
+                                    width: 120,
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(100),
+                                        image: DecorationImage(
+                                            image: AssetImage(imageCarousel[i]),
+                                            fit: BoxFit.cover))),
+                                Container(
+                                    padding: const EdgeInsets.only(left: 30),
+                                    height: 100,
+                                    width: swidth - 160,
+                                    child: ListView(children: [
+                                      Text(titleCarousel[i],
+                                          style: textTheme.displayMedium),
+                                      Container(height: 35),
+                                      Text(nameCarousel[i],
+                                          style: textTheme.displaySmall),
+                                      Container(height: 20),
+                                      Text(summaryCarousel[i],
+                                          style: textTheme.displaySmall)
+                                    ])),
+                              ]),
+                            },
+                          ],
+                        )
                       ]),
-                      Container(width: swidth * 0.25),
-                      TextButton(
-                          style: ButtonStyle(
-                              fixedSize: MaterialStatePropertyAll(Size(150, 50)),
+                  Container(
+                      color: CoresPersonalizadas.azulObama,
+                      height: 180,
+                      width: swidth * 0.95,
+                      child: Column(children: [
+                        Row(children: [
+                          Container(width: 15),
+                          const SizedBox(
+                              width: 50,
+                              height: 100,
+                              child: Icon(
+                                Icons.phone,
+                                size: 40,
+                                color: background,
+                              )),
+                          SizedBox(
+                              width: swidth * 0.7,
+                              height: 100,
+                              child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.center,
+                                  children: [
+                                    Text('REQUEST A FREE QUOTE',
+                                        style: textTheme.displayMedium),
+                                    Text(
+                                        'Get answers and advice from people you want ir from.',
+                                        style: textTheme.displaySmall)
+                                  ]))
+                        ]),
+                        Container(width: swidth * 0.25),
+                        TextButton(
+                            style: ButtonStyle(
+                              fixedSize:
+                                  MaterialStatePropertyAll(Size(150, 50)),
                               backgroundColor:
                                   MaterialStatePropertyAll(onPrimary),
-                              shape: MaterialStatePropertyAll(
-                                  CircleBorder(eccentricity: 100))),
-                          onPressed: () {},
-                          child:
-                              Text('CONTACT US', style: textTheme.labelMedium)),
-                    ]))),
+                              overlayColor:
+                                  MaterialStatePropertyAll(onPrimary),
+                            ),
+                            onPressed: () {},
+                            child: Text('CONTACT US',
+                                style: textTheme.labelMedium)),
+                      ])),
+                ])),
             SizedBox(width: swidth * 0.9, height: 40),
             SizedBox(
                 width: swidth * 0.8,
@@ -342,7 +349,8 @@ class Carousel extends StatelessWidget {
                         decoration: BoxDecoration(
                             border: Border.all(
                                 width: 6,
-                                color: const Color.fromARGB(255, 231, 228, 228))),
+                                color:
+                                    const Color.fromARGB(255, 231, 228, 228))),
                         child: Image(
                             image: AssetImage(partnersLogo[i]),
                             fit: BoxFit.contain)),
